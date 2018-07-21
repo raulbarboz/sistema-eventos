f9123d396419663cea8dcc3a14064fd171a55e72
Raul Barboza <ruzito@gmail.com>
2018-07-20 22:04:51 -0300

    redux101

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -5,20 +5,51 @@ const incrementCount = ({ incrementBy = 1 } = {}) => ({
   incrementBy
 })
 
-const store = createStore((state = { count: 0 }, action) => {
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+})
+
+const setCount = ({ setTo = 1 } = {}) => ({
+  type: 'SET',
+  setTo
+})
+
+const resetCount = () => ({
+  type: 'RESET'
+})
+
+const countReducer = (state = { count: 0 }, action) => {
   switch(action.type){
     case 'INCREMENT':
-      return{
-        count: state.count + action.incrementBy
-      };
-      default:
-      return state;
+     return {
+       count: state.count + action.incrementBy
+     };
+    case 'DECREMENT':
+     return {
+       count: state.count - action.decrementBy
+     }
+    case 'SET':
+     return {
+       count: action.setTo
+     }
+    case 'RESET':
+     return {
+       count: 0
+     }
+    default:
+    return state;
   }
-})
+}
+
+const store = createStore(countReducer)
 
 console.log(store.getState());
 
 store.dispatch(incrementCount({ incrementBy:5 }))
+store.dispatch(decrementCount({ decrementBy:4 }))
+store.dispatch(setCount({ setTo:4 }))
+store.dispatch(resetCount())
 
 console.log(store.getState());
 
