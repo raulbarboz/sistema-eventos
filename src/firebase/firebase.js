242d5151567ceb51274c8aa69a4f82f9897d3772
Raul Barboza <ruzito@gmail.com>
2018-07-26 22:19:07 -0300

    pushing to firebase done

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,39 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyCCpSBPZmrWX2GDdnJ9MTrEZLw4OTM9SRQ",
-  authDomain: "expensify-app-9da7d.firebaseapp.com",
-  databaseURL: "https://expensify-app-9da7d.firebaseio.com",
-  projectId: "expensify-app-9da7d",
-  storageBucket: "expensify-app-9da7d.appspot.com",
-  messagingSenderId: "614404121319"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
+const database = firebase.database();
 
-firebase.database().ref().set({
-  name: 'Raul Barboza'
-})
+export { firebase, database as default };
+
+
+// database.ref('expenses').on('value', (dataSnapshot) => {
+//     const expenses = [];
+//     dataSnapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       })
+//     })
+//     console.log(expenses);
+//   })
+
+
+// database.ref('expenses').push({
+//   description: 'suco',
+//   note: 'suco laranja',
+//   amount: 600,
+//   createdAt: 976123498740
+// })
+
+// firebase.database().ref('expenses').set({
+//   name: 'Raul Barboza'
+// })
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+require('dotenv').config({path: '.env'})
 
 module.exports = (env) => {
   const isProduction = env === 'production';
@@ -37,7 +39,15 @@ module.exports = (env) => {
       }]
     },
     plugins: [
-      CSSExtract
+      CSSExtract,
+      new webpack.DefinePlugin({
+        'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+        'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+        'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+        'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+        'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+        'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID)
+      })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
