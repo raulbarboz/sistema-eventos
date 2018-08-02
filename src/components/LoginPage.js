7af1f17be780dd99c1f56548cbc60c9de85ea3d8
Raul Barboza <ruzito@gmail.com>
2018-08-01 22:41:30 -0300

    googleAuth done

---
+++ src/components/LoginPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginPage = ({startLogin}) => (
+    <div>
+        <input type="email"></input>
+        <input type="password"></input>
+        <button
+            onClick={startLogin}
+        >Login</button>
+    </div>
+)
+
+
+const mapDispatchToProps = (dispatch) => ({
+    startLogin: () => dispatch(startLogin())
+})
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
