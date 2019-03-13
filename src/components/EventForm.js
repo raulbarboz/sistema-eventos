ca24db0395b2637861257799059c0526c16cb7b8
Raul Barboza <ruzito@gmail.com>
2019-03-13 13:30:46 +0000

    fixed DateRangerPicker with minimumDays 0

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -90,6 +90,7 @@ export default class EventForm extends React.Component {
                       onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
                       focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                       onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                      minimumNights={0}
                     />
                     <textarea
                     placeholder="Descricão do Evento"
