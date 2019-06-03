7fa310d70fa26dad9f13cc5c8b403040b0f5d85c
Raul Barboza <ruzito@gmail.com>
2019-06-02 22:36:06 -0300

    src/components/home/Search.js creating inputSearc 80% done

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../../actions/filters';
+import selectEvents from '../../selectors/events';
+
+class InputSearch extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    handlerInput(e){
+        const inputText = e.target.value;
+        console.log(this.props)
+        if(inputText){
+            
+        }
+    }
+    render(){
+        return(
+            <input type="text" name="input-search" onChange={this.handlerInput}/>
+        )
+    }
+}
 
 class EventParent extends React.Component {
     constructor(props){
@@ -27,22 +47,29 @@ class Search extends React.Component {
     render(){
         return(
             <div>
+            <InputSearch />
             { this.props.eventsStuffs.map((event, key) => {
                 if(event.event_type === "events"){
                     return <EventParent key={event.event_id} {...event} />
                 }       
              })}
             </div>
-            
         )
     }
 }
 
-const mapStateToProps = (state, props) => {
+const mapDispatchToProps = (text) => {
     return{
-     // events:selectEvents(state.events, state.filters)
-      eventsStuffs:state.eventsStuffs
+        dispatchFilter: (text) => dispatch(setTextFilter(text))
+    }
+}
+
+const mapStateToProps = (state) => {
+    return{
+      // events:selectEvents(state.events, state.filters)
+      filters: state.filters,
+      eventsStuffs:selectEvents(state.eventsStuffs ,state.filters)
     }
   }
   
-  export default connect(mapStateToProps)(Search);
+  export default connect(mapStateToProps, mapDispatchToProps)(Search);
